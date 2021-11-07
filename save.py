9ef22bf320293d9e498b700309e90760f26cf9db
Krzystof Staroń <kisiel3141@gmail.com>
2021-11-07 21:41:14 +0100

    deafult colors

--- save.py
+++ save.py
@@ -8,10 +8,10 @@ def save(colors):
 
     for i in range(len(colors)):
         hex = colors[i].lstrip('#')
-        print(hex)
         rgbColors[i] = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
     
     size = int(math.sqrt(len(colors)))
     im = Image.new('RGB', (size, size))
     im.putdata(rgbColors)
     im.save('test.png')
+    print("saved")
